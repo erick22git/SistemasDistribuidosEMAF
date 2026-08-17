0fba1c46867c5706ec254227f5d25b12b53bc6d2
erick <erickarancibia77@gmail.com>
2026-08-17 14:58:02 -0400

    Agrega codigo fuente de Practica 1 - Sockets Multihilo
    Servidor secuencial y multihilo, clientes basico y de chat, manejo de salas.

---
+++ Tareas/Practicas 1. Soquets Multihilo/ServidorSecuencial.java
@@ -0,0 +1,34 @@
+package com.mycompany.maven;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author erick
+ */
+import java.io.*;
+import java.net.*;
+public class ServidorSecuencial {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket servidor = new ServerSocket(5000);
+        System.out.println("Servidor escuchando en el puerto 5000...");
+        while (true) {
+            System.out.println("accept() ... esperando un cliente");
+            Socket cliente = servidor.accept(); // BLOQUEA
+            System.out.println("Conectado: " + cliente.getInetAddress());
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(cliente.getInputStream()));
+            PrintWriter out = new PrintWriter(cliente.getOutputStream(), true);
+            String linea;
+            while ((linea = in.readLine()) != null) { // BLOQUEA
+                System.out.println("Recibido: " + linea);
+                out.println("ECO: " + linea);
+            }
+            cliente.close();
+        }
+    }
+}
